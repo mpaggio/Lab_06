42e5822649faef0e8a56f48be169e415c809a188
Marco Paggetti <marco.paggetti@studio.unibo.it>
2023-11-05 17:31:23 +0100

    Trying resolving part 3) of README file

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/api/NetworkException.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/api/NetworkException.java
@@ -1,12 +1,15 @@
 package it.unibo.exceptions.fakenetwork.api;
 
 import java.util.Objects;
-import java.util.IOException;
+import java.io.IOException;
 
 public class NetworkException extends IOException{
     
     public NetworkException(){
-        IOException ioexception = new IOException();
-        ioexception.message = "Network error: no response";
+        super("Network error: no response");
+    }
+
+    public NetworkException(String message){
+        super("Network error while sending message: " + message);
     }
 }
