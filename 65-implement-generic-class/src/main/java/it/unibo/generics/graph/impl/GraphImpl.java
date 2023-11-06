3eb0f8d95aa5929debc256729aa09fd14f04a6f5
Marco Paggetti <marco.paggetti@studio.unibo.it>
2023-11-06 11:43:00 +0100

    Created 2 methods of Graph interface: addNode() and addEdge()

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -2,17 +2,42 @@ package it.unibo.generics.graph.impl;
 
 import java.util.List;
 import java.util.Set;
+import java.util.Objects;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class GraphImpl<N> implements Graph<N>{
+    private final Map<N,Set<N>> edgesMap = new LinkedHashMap<>();
 
-    public void addNode(N node);
+    /* Adds a node: nothing happens if node is null or already there. */
+    public void addNode(N node){
+        if(Node != null && !this.edgesMap.contains(Node)){
+            this.edgesMap.put(Node, new HashSet<>());
+        }
+    }
 
-    public void addEdge(N source, N target);
+    /* Adds an edge: nothing happens if source or target are null. */
+    public void addEdge(N source, N target){
+        if(source != null && target != null){
+            if(this.edgesMap.contains(source) && this.edgesMap.contains(target)){
+                this.edgesMap.get(source).add(target);
+            }
+        }
+    }
 
-    public Set<N> nodeSet();
+    /* Return all the nodes */
+    public Set<N> nodeSet(){
 
-    public Set<N> linkedNodes(N node);
+    }
 
-    public List<N> getPath(N source, N target);
+    /* Returns all the nodes directly targeted from a node. */
+    public Set<N> linkedNodes(N node){
+
+    }
+
+    /* Gets one sequence of nodes connecting source to target. */
+    public List<N> getPath(N source, N target){
+
+    }
 
 }
