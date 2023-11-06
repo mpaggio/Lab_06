6b29ccdf94f9b4767576158b42f921f2db93869f
Marco Paggetti <marco.paggetti@studio.unibo.it>
2023-11-06 11:53:47 +0100

    Corrected addNode() and addEdge() (Errors catched with gradle)

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -4,6 +4,7 @@ import it.unibo.generics.graph.api.Graph;
 import java.util.List;
 import java.util.Set;
 import java.util.Objects;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -12,15 +13,15 @@ public class GraphImpl<N> implements Graph<N>{
 
     /* Adds a node: nothing happens if node is null or already there. */
     public void addNode(N node){
-        if(Node != null && !this.edgesMap.contains(Node)){
-            this.edgesMap.put(Node, new HashSet<>());
+        if(node != null && !this.edgesMap.containsKey(node)){
+            this.edgesMap.put(node, new HashSet<>());
         }
     }
 
     /* Adds an edge: nothing happens if source or target are null. */
     public void addEdge(N source, N target){
         if(source != null && target != null){
-            if(this.edgesMap.contains(source) && this.edgesMap.contains(target)){
+            if(this.edgesMap.containsKey(source) && this.edgesMap.containsKey(target)){
                 this.edgesMap.get(source).add(target);
             }
         }
@@ -28,17 +29,17 @@ public class GraphImpl<N> implements Graph<N>{
 
     /* Return all the nodes */
     public Set<N> nodeSet(){
-
+        return null;
     }
 
     /* Returns all the nodes directly targeted from a node. */
     public Set<N> linkedNodes(N node){
-
+        return null;
     }
 
     /* Gets one sequence of nodes connecting source to target. */
     public List<N> getPath(N source, N target){
-
+        return null;
     }
 
 }
